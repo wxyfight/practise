38faadf87691bc4e5352320c372b4f40770a6f79
wangxy <wxy_fight@126.com>
2018-10-23 14:12:48 +0800

    add of two linkedlist

---
+++ DataStructure/src/com/wxy/leetcode_simple/Test2.java
@@ -0,0 +1,31 @@
+package com.wxy.leetcode_simple;
+
+/**
+ * addTwoNumbers
+ * @author wangxy
+ */
+public class Test2 {
+	public static void main(String[] args) {
+		
+	}
+	
+	 public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode(int x) { val = x; }
+	 }
+	//https://leetcode-cn.com/problems/add-two-numbers/description/ 
+	class Solution {
+	    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	        ListNode last;
+	    	while(l1.next!=null){
+	        	
+	        }
+	        
+	    	
+	    	
+	    	
+	    	return null;
+	    }
+	}	
+}
